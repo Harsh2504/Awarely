5b6caddb9e4cc384ae3c888a06dfddb4ac13da85
Harsh2504 <harsh2504patil@gmail.com>
2025-05-06 23:31:39 +0530

    Final Push before deployment 2.0

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,13 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
   build: {
     outDir: 'build', // or any other directory name
+  },
+  server: {
+    host:true
   },
 });
